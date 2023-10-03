91c38469c1c5395f04a6f5e8f4c2196bcadda4f3
CarlosMoreno3 <101369176+CarlosMoreno3@users.noreply.github.com>
2023-10-03 17:20:49 -0600

    Finalizacion de Practica semana 3

---
+++ src/main/java/com/Practica2S3/Practica2S3Application.java
@@ -0,0 +1,13 @@
+package com.Practica2S3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Practica2S3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Practica2S3Application.class, args);
+	}
+
+}
